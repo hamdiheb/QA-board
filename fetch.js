256be8b4c1eb97d9170a2fa0e94894e2eb2ece99
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-05-16 10:45:57 +0200

    Submit function

--- fetch.js
+++ fetch.js
@@ -2,25 +2,24 @@ const server = 'localhost'
 const port = 3000
 
 export default async function fetchQuestions(server,port) {
-  const req = await fetch(`${server}:${port}\questions`)
-  const res = await req.json()
-
-  if (!res) {
-    console.log('error')
-  } else {
-    console.log(res)
-  }
+    const req = await fetch(`${server}:${port}\questions`)
+    const res = await req.json()
+    return res;
 }
 
-export default async function postQuestion(server,port){
-    const question = document.querySelector('#question')
-    const vote = document.querySelector('#vote')
+export default async function postQuestion(server,port,question){
+    
     const req = await fetch(`${server}:${port}\question`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-type': 'application/json'
-        },
+            },
         body: JSON.stringify({question: question})
-    });
+        });
+}
+
+export default async function postVote(){
+
+
 }
